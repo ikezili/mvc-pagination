6330a2846857f0ae8fc251d72a1eaac2a13200c6
Leandro Ikezili <leandro.ikezili@gmail.com>
2014-10-06 08:59:33 -0300

    demo

---
+++ demo/MVCPagination/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVCPagination
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery/jquery-2.0.3.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-mvcPagination").Include("~/Scripts/jquery.mvcPagination/jquery.mvcPagination.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap/3.0.3/css/bootstrap.min.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
